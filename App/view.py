1181ea0cba2c7473bacf898c0674836e45f015ec
Jeronimo Vargas <j.vargasr2@uniandes.edu.co>
2021-08-24 21:24:28 -0500

    Cambios finales

--- App/view.py
+++ App/view.py
@@ -48,7 +48,7 @@ def loadBooks():
     """
     Carga los libros
     """
-    return controller.loadBooks('GoodReads/books-small.csv')
+    return controller.loadBooks('GoodReads/books.csv')
 
 def loadTags():
     """
@@ -61,7 +61,7 @@ def loadBookTags():
     Cargar los Tags de libros
     """
     # TODO: Modificación de Est-1 en el Lab 2
-    return controller.loadTags('GoodReads/book_tags-small.csv')
+    return controller.loadTags('GoodReads/book_tags.csv')
     
 
 """
